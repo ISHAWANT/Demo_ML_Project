75be0c058f44cbb0a2928747afc81c8c2e8d3587
ishawant kumar <ishawantkumar@gmail.com>
2023-02-16 16:28:35 +0530

    Housing log tested in app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask,jsonify,render_template 
-
+from housing.logger import logging 
 app = Flask(__name__) 
 
 @app.route("/",methods=['GET','POST']) 
 
 def index():
+    logging.info('We are testing logging module')
     return "Starting Mahcine learning Project"
 
 if __name__=="__main__":
