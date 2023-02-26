e302a60d0cabf6918fe5fc90da2f12192cbe47f6
ishawant kumar <ishawantkumar@gmail.com>
2023-02-26 12:16:45 +0530

    demo.py done for data ingestion

---
+++ demo.py
@@ -0,0 +1,14 @@
+from housing.pipeline.pipeline import Pipeline
+from housing.exception import HousingExeption
+from housing.logger import logging
+ 
+def main():
+    try:
+        pipeline = Pipeline
+        pipeline.run_pipeline()
+    except Exception as e:
+        logging.error(f"{e}")
+        print(e)  
+     
+if __name__=="__main__":
+    main() 
